4a9cd1c3009eb62f4c7b8267c2aa56d8ca504767
huangyue <huangyue@sinodata.net.cn>
2022-08-12 15:48:26 +0800

    test: 测试代码同步推送两个仓库

--- src/pages/Menu/Menu-2/Menu-2-1/index.tsx
+++ src/pages/Menu/Menu-2/Menu-2-1/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import userStore from '@/store/userStore';
 
 const Menu_2_1 = () => {
-  const { token, userInfo } = userStore();
   return <div style={{ border: '1px solid red' }}>菜单2-1</div>;
 };
 export default Menu_2_1;
