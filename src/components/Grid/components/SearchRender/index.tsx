e49208b893da30e9b72b8775e0e15d976c2ad4db
huangyue <huangyue@sinodata.net.cn>
2022-10-18 17:59:22 +0800

    feat: 新增grid组件

---
+++ src/components/Grid/components/SearchRender/index.tsx
@@ -0,0 +1,123 @@
+import moment from 'moment';
+import type { ColProps } from 'antd/lib/grid/col';
+import { Input, Form, DatePicker, TreeSelect, Col, Select } from 'antd';
+
+const gridLayout = {
+  xxl: { span: 6 },
+  xl: { span: 8 },
+  lg: { span: 12 },
+  md: { span: 24 },
+};
+
+const SearchRender = (data: any, searchGridLayout: ColProps = gridLayout) => {
+  return (data || []).map((field: any) => {
+    const basicAttr = {
+      label: field.label,
+      name: field.name,
+    };
+    switch (field.type) {
+      case 'dateTimePicker':
+        return (
+          <Col {...searchGridLayout} key={field.name}>
+            <Form.Item {...basicAttr}>
+              <DatePicker.RangePicker
+                allowEmpty={field.allowEmpty || [false, false]}
+                allowClear
+                showTime={field.showtime}
+                disabled={field.disabled}
+                style={{ width: '100%' }}
+                ranges={{
+                  今日: [moment().startOf('day'), moment().endOf('day')],
+                  最近7日: [moment().subtract(7, 'd'), moment()],
+                  最近30日: [moment().subtract(30, 'days'), moment()],
+                  最近整月: [
+                    moment().subtract(1, 'months').startOf('month'),
+                    moment().subtract(1, 'months').endOf('month'),
+                  ],
+                }}
+              />
+            </Form.Item>
+          </Col>
+        );
+      case 'datePicker':
+        return (
+          <Col {...searchGridLayout} key={field.name}>
+            <Form.Item {...basicAttr}>
+              <DatePicker
+                picker={field.picker ? field.picker : undefined}
+                style={{ width: '100%' }}
+              />
+            </Form.Item>
+          </Col>
+        );
+      case 'tree':
+        return (
+          <Col {...searchGridLayout} key={field.name}>
+            <Form.Item {...basicAttr}>
+              <TreeSelect
+                treeData={field.data}
+                disabled={field.disabled}
+                allowClear
+                showSearch
+                treeNodeFilterProp="title"
+              />
+            </Form.Item>
+          </Col>
+        );
+      case 'multipleTree':
+        return (
+          <Col {...searchGridLayout} key={field.name}>
+            <Form.Item {...basicAttr}>
+              <TreeSelect
+                treeData={field.data}
+                disabled={field.disabled}
+                allowClear
+                showSearch
+                treeNodeFilterProp="title"
+                multiple
+              />
+            </Form.Item>
+          </Col>
+        );
+      case 'select':
+        return (
+          <Col {...searchGridLayout} key={field.name}>
+            <Form.Item {...basicAttr}>
+              <Select
+                allowClear
+                mode={field.mode ? field.mode : undefined}
+                showSearch={field.showSearch ? field.showSearch : false}
+                filterOption={field.filterOption ? field.filterOption : false}
+                optionFilterProp={
+                  field.optionFilterProp ? field.optionFilterProp : undefined
+                }
+                listHeight={400}
+              >
+                {(field.data || []).map((option: any) => {
+                  return (
+                    <Select.Option
+                      value={option.value}
+                      key={option.value}
+                      lable={option.title || option.label}
+                    >
+                      {option.title || option.label}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </Form.Item>
+          </Col>
+        );
+      default:
+        return (
+          <Col {...searchGridLayout} key={field.name}>
+            <Form.Item {...basicAttr}>
+              <Input disabled={field.disabled} />
+            </Form.Item>
+          </Col>
+        );
+    }
+  });
+};
+
+export default SearchRender;
---
+++ src/components/Grid/index.tsx
@@ -0,0 +1,55 @@
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { Button, Col, Form, Input, Row, Select, Card } from 'antd';
+import React, { useState } from 'react';
+import SearchRender from './components/SearchRender';
+
+const { Option } = Select;
+
+const Grid = (props: any) => {
+  const [expand, setExpand] = useState(false);
+  const [form] = Form.useForm();
+  /**
+   *@description:
+   *@searchData 表单查询数据
+   *@searchButtonPosition  按钮位置，left 左边， center 中间，right（默认右边）
+   *@tableDataApi  请求数据接口
+
+   * */
+  const { searchData, searchButtonPosition = 'right' } = props;
+
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <Card bordered={false}>
+      <Form
+        form={form}
+        name="serarchForm"
+        className="serarchForm"
+        onFinish={onFinish}
+      >
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          {SearchRender(searchData)}
+        </Row>
+        <Row>
+          <Col span={24} style={{ textAlign: searchButtonPosition }}>
+            <Button type="primary" htmlType="submit">
+              查询
+            </Button>
+            <Button
+              style={{ margin: '0 8px' }}
+              onClick={() => {
+                form.resetFields();
+              }}
+            >
+              重置
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Card>
+  );
+};
+
+export default Grid;
